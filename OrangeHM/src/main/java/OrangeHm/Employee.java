fa26d220cf9fc55ad26053a5df46c6cad90d38b0
Shradhasshedbale <sshedbale2802@gmail.com>
2017-12-18 20:34:27 +0000

    OrangeHM/src/test/java/OrangeHm/features/DeleteEmployee.feature

--- OrangeHM/src/main/java/OrangeHm/Employee.java
+++ OrangeHM/src/main/java/OrangeHm/Employee.java
@@ -6,36 +6,43 @@ import org.openqa.selenium.support.FindBy;
 /**
  * Created by SHRADHA on 17/08/2017.
  */
-public class Employee extends BaseMain{
+public class Employee extends BaseMain {
 
 
-    @FindBy(xpath=".//*[@id='firstName']")
+    @FindBy(xpath = ".//*[@id='firstName']")
     private WebElement firstNameField;
 
-    @FindBy(xpath=".//*[@id='middleName']")
+    @FindBy(xpath = ".//*[@id='middleName']")
     private WebElement middleNameField;
 
-    @FindBy(xpath=".//*[@id='lastName']")
+    @FindBy(xpath = ".//*[@id='lastName']")
     private WebElement lastNameField;
 
-    @FindBy(xpath=".//*[@id='employeeId']")
+    @FindBy(xpath = ".//*[@id='employeeId']")
     private WebElement employeeIdField;
 
-    @FindBy(xpath=".//*[@id='photofile']")
+    @FindBy(xpath = ".//*[@id='photofile']")
     private WebElement addPhotoButton;
 
-    @FindBy(xpath=".//*[@id='btnSave']")
+    @FindBy(xpath = ".//*[@id='btnSave']")
     private WebElement addEmployeeSaveButton;
 
 
-
-public void add_new_employee(String add_name)
-    {
-        if (firstNameField.getText().trim().contains(add_name))
-        {
+    public void add_new_employee(String add_name) {
+        if (firstNameField.getText().trim().contains(add_name)) {
             firstNameField.sendKeys(add_name);
         }
     }
+
+
+    public void delete_employee(String del_name)
+
+    {
+        if (lastNameField.isEnabled())
+        {
+            lastNameField.clear();
+        }
+
+    }
+
 }
-
-
